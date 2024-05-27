62dee3f714a3f24d200eb069d2db87f9c9f0ea0d
MorenaLopezObejero <163904002+MorenaLopezObejero@users.noreply.github.com>
2024-05-27 15:49:01 -0300

    LOGINMANAGER Codigo

--- Assets/LOGINMANAGER.cs
+++ Assets/LOGINMANAGER.cs
@@ -1,9 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LOGINMANAGER : MonoBehaviour
 {
+    //Cuando el usuario presione Btn_Aceptar
+    //Debe escribirse un mensaje en la consola
+    //si el texto del InputPassword coincide
+    //con una contraseña predefinida por ustedes 
+    //escribir "Acces Granted"
+    //sino escribir "Access Denied"
+
+    public InputField Input_Contraseña;
+    public string Password;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +25,17 @@ public class LOGINMANAGER : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void contraseña()
+    {
+        if (Input_Contraseña.text == Password)
+        {
+            Debug.Log("Acces Granted");
+        }
+        else
+        {
+            Debug.Log("Access Denied");
+        }
     }
 }
